e5d2b1f64612e8b22c9459fc01f0763948fe0fe7
GustavoSwDaniel <gustavodanieldetoledo@gmail.com>
2023-06-08 14:21:14 -0300

    Improvements: new Improvements

--- src/components/Form/Register.vue
+++ src/components/Form/Register.vue
@@ -3,105 +3,106 @@
 div(class="main-container")
     div.box(class="register-container") 
         h2.mb-5.title.is-4 Registro de usuario
-        div.field.is-horizontal
-            div.field-label.is-normal
-                label.label Email
-            div.field-body
-                div.field
-                    p.control.is-expanded.has-icons-left
-                        input.input(type="email" v-model="email" placeholder="e.g. alex@example.com")
-                        span.icon.is-small.is-left
-                            font-awesome-icon(icon="fa-regular fa-envelope" color="#832727")
-        
-        div.field.is-horizontal
-            div.field-label.is-normal
-                label.label Password
-            div.field-body
-                div.field
-                    p.control.is-expanded.has-icons-left
-                        input.input(type="password" v-model="password" placeholder="********")
-                        span.icon.is-small.is-left
-                            font-awesome-icon(icon="fa-solid fa-lock" color="#832727")
-                div.field
-                    p.control.is-expanded.has-icons-left
-                        input.input(type="Password" placeholder="Confirmar senha")
-                        span.icon.is-small.is-left
+        form(@submit="registerUser")
+            div.field.is-horizontal
+                div.field-label.is-normal
+                    label.label Email
+                div.field-body
+                    div.field
+                        p.control.is-expanded.has-icons-left
+                            input.input(type="email" v-model="email" placeholder="e.g. alex@example.com" required)
+                            span.icon.is-small.is-left
+                                font-awesome-icon(icon="fa-regular fa-envelope" color="#832727")
+
+            div.field.is-horizontal
+                div.field-label.is-normal
+                    label.label Password
+                div.field-body
+                    div.field
+                        p.control.is-expanded.has-icons-left
+                            input.input(type="password" v-model="password" placeholder="********" required)
+                            span.icon.is-small.is-left
                                 font-awesome-icon(icon="fa-solid fa-lock" color="#832727")
-        
-        div.field.is-horizontal
-            div.field-label.is-normal
-                label.label Nome
-            div.field-body
-                div.field
-                    input.input(v-model="name_user" type="text" placeholder="Nome")
-                div.field
-                    input.input(v-model="last_name" type="text" placeholder="Sobrenome")
+                    div.field
+                        p.control.is-expanded.has-icons-left
+                            input.input(type="Password" v-model="confirm_passoword" placeholder="Confirmar senha" required)
+                            span.icon.is-small.is-left
+                                font-awesome-icon(icon="fa-solid fa-lock" color="#832727" required)
 
-        div.field.is-horizontal
-            div.field-label.is-normal
-                label.label CPF
-            div.field-body
-                div.field      
-                    input.input(v-model="cpf" type="text" placeholder="Cpf")
-        
-        div.field.is-horizontal
-            div.field-label.is-normal
-                label.label Endereço
-            div.field-body
-                div.field      
-                    input.input(v-model="street" type="text" placeholder="Rua")
-                div.field      
-                    input.input(v-model="city" type="text" placeholder="Cidade")
-        
-        div.field.is-horizontal
-            div.field-label.is-normal
-                label.label Estado
-            div.field-body
-                div.field.is-narrow
-                    p.control.is-expanded.has-icons-left
-                        div.select.is-fullwidth
-                            select(v-model="state")
-                                option(v-for="state in this.states" :value="state.text") {{ state.text }} 
-                div.field      
-                    input.input(v-model="village" type="text" placeholder="Vila")
-                div.field  
-                    input.input(v-model="complement" type="text" placeholder="Complemento")
+            div.field.is-horizontal
+                div.field-label.is-normal
+                    label.label Nome
+                div.field-body
+                    div.field
+                        input.input(v-model="name_user" type="text" placeholder="Nome" required)
+                    div.field
+                        input.input(v-model="last_name" type="text" placeholder="Sobrenome" required)
 
-        div.field.is-horizontal
-            div.field-label.is-normal
-                label.label Cep
-            div.field-body
-                div.field  
-                    input.input(v-model="cep" type="text" placeholder="Cep")
-        
-        div.field.is-horizontal
-            div.field-label.is-normal
-                label.label Telefone
-            div.field-body
-                div.field
-                    input.input(v-model="phone_number" type="text" placeholder="Telefone" ) 
-        
-        div.field.is-horizontal
-            div.field-label.is-normal
-                label.label Tipo de telefone 
-            div.field-body                 
-                div.field.is-narrow
-                    div.control
-                        label.radio
-                            input.radio(v-model="phone_type" type="radio" id="raio-input-cell" name="fav_language" value="Cell") 
-                            | Celular
-                        label.radio
-                            input.radio(v-model="phone_type" type="radio" id="raio-input-cell" name="fav_language" value="Tell") 
-                            | Telefone
-        
-        div(class="actions-container")
-            div(class="actions")
-                button.mt-5(v-on:click.prevent="registerUser($event)") Registrar
-                div(v-show="regiter_error" class="register_error")
-                    p(class="register_error_msg") Todos os campos devem ser preenchidos
-                div(class="link-login")
-                    p Ja possui cadastra? 
-                    a(href="/login") ENTRAR
+            div.field.is-horizontal
+                div.field-label.is-normal
+                    label.label CPF
+                div.field-body
+                    div.field      
+                        input.input(v-model="cpf" type="text" placeholder="Cpf" required)
+
+            div.field.is-horizontal
+                div.field-label.is-normal
+                    label.label Endereço
+                div.field-body
+                    div.field      
+                        input.input(v-model="street" type="text" placeholder="Rua" required)
+                    div.field      
+                        input.input(v-model="city" type="text" placeholder="Cidade" required)
+
+            div.field.is-horizontal
+                div.field-label.is-normal
+                    label.label Estado
+                div.field-body
+                    div.field.is-narrow
+                        p.control.is-expanded.has-icons-left
+                            div.select.is-fullwidth
+                                select(v-model="state" required)
+                                    option(v-for="state in this.states" :value="state.text") {{ state.text }} 
+                    div.field      
+                        input.input(v-model="village" type="text" placeholder="Vila" required)
+                    div.field  
+                        input.input(v-model="complement" type="text" placeholder="Complemento" required)
+
+            div.field.is-horizontal
+                div.field-label.is-normal
+                    label.label Cep
+                div.field-body
+                    div.field  
+                        input.input(v-model="cep" type="text" placeholder="Cep" required)
+
+            div.field.is-horizontal
+                div.field-label.is-normal
+                    label.label Telefone
+                div.field-body
+                    div.field
+                        input.input(v-model="phone_number" type="text" placeholder="Telefone" required) 
+
+            div.field.is-horizontal
+                div.field-label.is-normal
+                    label.label Tipo de telefone 
+                div.field-body                 
+                    div.field.is-narrow
+                        div.control
+                            label.radio
+                                input.radio(v-model="phone_type" type="radio" id="raio-input-cell" name="fav_language" value="Cell" required) 
+                                | Celular
+                            label.radio
+                                input.radio(v-model="phone_type" type="radio" id="raio-input-tell" name="fav_language" value="Tell" required) 
+                                | Telefone
+
+            div(class="actions-container")
+                div(class="actions")
+                    button.mt-5(type="submit") Registrar
+                    div(v-show="regiter_error" class="register_error")
+                        p(class="register_error_msg") {{error_message}}
+                    div(class="link-login")
+                        p Ja possui cadastra? 
+                        a(href="/login") ENTRAR
 
 </template>
 
@@ -129,13 +130,28 @@ export default {
             'village': '',
             'complement': '',
             regiter_error: false,
-            states: states
+            error_message: 'Todos os campos devem ser preenchidos',
+            states: states,
+            confirm_passoword: ''
         }
     },
     methods: {
+        validatePasswords() {
+            if (this.password !== this.confirm_passoword) {
+                alert('As senhas não estão iguais');
+            }
+        },
         registerUser() {
-            if (!(this.email, this.last_name, this.name_user, this.cpf, this.phone_type, this.phone_number, this.password, this.street, this.cep, this.city, this.state, this.village, this.complement))
+            if (!(this.email, this.last_name, this.name_user, this.cpf, this.phone_type, this.phone_number, this.password, this.confirm_passoword, this.street, this.cep, this.city, this.state, this.village, this.complement)) {
                 this.regiter_error = true
+                return
+            }
+
+            if (!(this.password == !this.confirm_passoword)) {
+                this.regiter_error = true
+                this.error_message = "Password estão diferentes"
+                return
+            }
 
             let register_data = {
                 name: this.name_user,
@@ -166,6 +182,10 @@ export default {
                 })
                 .catch((error) => {
                     console.log(error);
+                    if (error.response.status === 422) {
+                        this.error_message = "Email ou cpf ja estão em uso"
+                        this.regiter_error = true
+                    }
                 });
         }
     }
@@ -175,24 +195,24 @@ export default {
 
 <style lang="scss" scoped>
 .main-container {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 }
 
 .register-container {
-  width: 68%;
+    width: 68%;
 }
 
 
-.actions-container{
+.actions-container {
     width: 100%;
     display: flex;
     justify-content: center;
 }
 
-.actions{
+.actions {
     width: 18%;
     display: flex;
     flex-direction: column;
@@ -242,11 +262,9 @@ button {
     margin-bottom: 20px;
 }
 
-.register_error{
+.register_error {
     display: flex;
     justify-content: center;
     align-content: center;
 }
-
-
 </style>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-div(id="main")
+div
   HeaderVue(:bannerIsVisible='false')
   RegisterVue
 </template>
